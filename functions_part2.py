b69983b205c339fccc2450112f9129dd0efeccd9
Meriem Zitouni <81829220+MerieemZitouni@users.noreply.github.com>
2022-01-06 17:42:49 +0100

    Update functions_part2.py

--- functions_part2.py
+++ functions_part2.py
@@ -1,12 +1,13 @@
 def prime_nb (number) :
- i = 2 
- verif = True
- while (i <= ((number // 2))) :
-   if ((number % i) == 0) : 
-    verif = False
-   i = i + 1 
- return (verif)
+ if (number > 0 ) :
+   i = 2 
+   verif = True
+   while (i <= ((number // 2))) :
+      if ((number % i) == 0) : 
+        verif = False
+      i = i + 1 
+   return (verif)
+ else :
+   return(False)
 n = int (input ("enter a number : "))
 print(prime_nb(n))
-
-  
