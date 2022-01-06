0c28bcee3a2f8dc109537010ceba8c62c45327b4
Meriem Zitouni <81829220+MerieemZitouni@users.noreply.github.com>
2022-01-06 18:21:27 +0100

    Update functions_part5.py

--- functions_part5.py
+++ functions_part5.py
@@ -1,11 +1,4 @@
 def reverse_string (str):
-    ch = str[len(str) - 1]
-    outp = ''
-    i = 1
-    while (i != len(str) + 1 ) :
-      outp = outp + ch
-      i = i + 1
-      ch =  str[len(str) - i] 
-    return(outp)
+ return(str[::-1])
 c  = input("enter a string : ")
 print(reverse_string(c))
